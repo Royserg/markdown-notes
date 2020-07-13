10229c3a5751d817888c2efc22aa027faedcadfa
Jakub <staniojakub@gmail.com>
2020-07-13 14:48:07 +0200

    Change Content on Home page

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import Page from 'components/Page/Page'
+import MarkdownDisplay from '../../components/MarkdownDisplay/MarkdownDisplay'
 
-import Typography from '@material-ui/core/Typography'
+const content = `
+  ## Markdown Notes
+  ### Create notes using markdown and divide them into categories
+`
 
 const Dashboard = props => {
 
   return (
     <Page navTitle='Home'>
-      <Typography variant='h5' gutterBottom>
-        Application for documenting what I've learnt during 4th semester of Computer Science.
-      </Typography>
-      <Typography variant='subtitle1' gutterBottom>
-        Choose from categories in the Sidebar.
-      </Typography>
+      <MarkdownDisplay data={content}/>
     </Page>
   )
 }
